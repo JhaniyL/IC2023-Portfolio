ac63ba69077c3b49fbd31334ebba0a8e857637ba
JhaniyL <137207845+JhaniyL@users.noreply.github.com>
2023-07-06 11:08:54 -0400

    Init commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let startButton = document.querySelector(".startButton");
+let exitButton = document.querySelector(".exitButton");
+let mainPage = document.querySelector(".mainPage");
+let startPage = document.querySelector(".startPage")
+
+startButton.onclick = function() {
+
+    startPage.style.display = "none";
+    mainPage.style.display = "block";
+  };
+
+  
+  
