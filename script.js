532c497fe5da9998de8cd02563b6893d02cf6f22
JhaniyL <137207845+JhaniyL@users.noreply.github.com>
2023-07-10 18:10:21 -0400

    Init commit

--- script.js
+++ script.js
@@ -3,17 +3,18 @@ let start = document.querySelector(".startButton");
 let exitButton = document.querySelector(".exitButton");
 let mainPage = document.querySelector(".mainPage");
 let startPage = document.querySelector(".startPage");
-let loadingScrn=document.querySelector(".loadingScrn");
+let loadingScrn = document.querySelector("#loadingScrn");
 
 
-start.onclick = function() {
-
-
+  start.onclick = function() {
     startPage.style.display = "none";
-    
-    mainPage.style.display = "block";
-  };
+    loadingScrn.style.display = "block";
 
+    setTimeout(function() {
+        loadingScrn.style.display = "none"; 
+        mainPage.style.display = "block"; 
+    }, 3000); 
+};
 
   
   function togglePopUp(popUpId) {
